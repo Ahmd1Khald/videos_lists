f265c4b96d3ce9fce4a9b7a3e5577d47fd2b3c04
Ahmd1Khald <ahmed.khaled.sayed2000@gmail.com>
2023-07-17 07:46:06 +0300

    generate topics_entity adapter

--- lib/Features/Videos_lists/domain/entites/topics_entity.dart
+++ lib/Features/Videos_lists/domain/entites/topics_entity.dart
@@ -1,9 +1,17 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 
+part 'topics_entity.g.dart';
+
+@HiveType(typeId: 0)
 class TopicsEntity extends Equatable {
+  @HiveField(0)
   final int itemCount;
+  @HiveField(1)
   final int id;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String mediaPath;
 
   const TopicsEntity({
---
+++ lib/Features/Videos_lists/domain/entites/topics_entity.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'topics_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TopicsEntityAdapter extends TypeAdapter<TopicsEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TopicsEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TopicsEntity(
+      id: fields[1] as int,
+      title: fields[2] as String,
+      itemCount: fields[0] as int,
+      mediaPath: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TopicsEntity obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.itemCount)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.mediaPath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TopicsEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
